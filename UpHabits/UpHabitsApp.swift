e3760ab318a1e9f7f80f9df04a7eb1881e814a64
Michael Miroshnikov <mishamiroshnikov054@gmail.com>
2025-07-25 21:29:45 +0200

    Initial Commit

---
+++ UpHabits/UpHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpHabitsApp.swift
+//  UpHabits
+//
+//  Created by Michael Miroshnikov on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct UpHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
