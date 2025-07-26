f3db3536844335e281ba97f42e42e82886c6c212
Michael Miroshnikov <mishamiroshnikov054@gmail.com>
2025-07-26 13:39:48 +0200

    testbuild

---
+++ UpHabits/Views/HomeView.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+import Lottie
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Home")
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+        }
+    }
+}
---
+++ UpHabits/Views/SettingsView.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+        }
+    }
+}
